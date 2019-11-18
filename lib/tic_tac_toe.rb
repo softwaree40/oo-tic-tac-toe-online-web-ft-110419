6b581651e1c359cf3e22d783aac04fd4dca3d50a
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-18 18:30:43 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -2,12 +2,12 @@ require 'pry'
 class  TicTacToe
     WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[4,7,1],[2,5,8],[0,4,8],[6,4,2]]
   def initialize()
-    @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+    @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]     #initialize when new game is created
     
   end
   
   def display_board
-        puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+        puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "  #board display 
         puts "-----------"
         puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
         puts "-----------"
@@ -16,13 +16,13 @@ class  TicTacToe
   end
   
   def input_to_index(index)
-      index.to_i-1
+      index.to_i - 1                                               #input from users
       #bi1nding.pry
 
   end
                                             #  location = 0     token = x
                                             
-    def move(location, token = "X")        #      move( 0 , O )
+    def move(location, token="X")        #      move( 0 , O )            
     @board[location] = token
     #binding.pry
   end
@@ -32,18 +32,39 @@ class  TicTacToe
        
     end
      def valid_move?(index)
-       !position_taken?(index) && @board.size > index
+       !position_taken?(index) && index.between?(0,8) #if postion is not taking and board size between range of 0 to 8 than input from users  is a valide move 
        #binding.pry
        
      end
      def turn_count
-        @board.count {|player| player == "X" || player == "O"}
+        @board.count {|player| player == "X" || player == "O"} #this is going to return amount of time X and O exist 
         #binding.pry
      end
      def current_player   #return correct player X for the third move 
-         
-         #binding.pry
+         turn_count % 2 == 0 ? "X" : "O"
+     end
+    def turn
+        input = gets.strip               #The placement of X on board 
+        index = input_to_index(input)
+        if valid_move?(index)   
+          move(index,current_player)                               #if valide_move is true then i can move to play game
+        else
+          turn 
+        end
+        display_board 
+    end
+    
+     def won?
+        combo = false
+         WIN_COMBINATIONS.each do |array|
+            if position_taken?(array[0]) && @board[array[0]] == @board[array[1]] && @board[array[1]] == @board[array[2]]
+              combo = array
+              return combo
+            end
+         end
+         combo
+     end
+     def full?  #method want me to return true for draw 
        
      end
-    
-end
+  end
